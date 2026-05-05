0b2f55bd2772fca840ff78d0fd75911e84664958
Gunhee0503 <gunhee.lee8@gmail.com>
2026-05-05 16:49:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // VOCA TREE Service Worker
 // 캐시 버전 (앱 업데이트 시 이 숫자를 올리세요)
-const CACHE_VERSION = 'v1.0.0';
+const CACHE_VERSION = 'v1.2.0';
 const CACHE_NAME = 'vocatree-' + CACHE_VERSION;
 
 // 오프라인에서도 작동하게 미리 캐시할 파일들
@@ -10,7 +10,8 @@ const PRECACHE_URLS = [
   './manifest.json',
   './icon.svg',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './pif_logo.jpg'
 ];
 
 // === 설치: 핵심 파일 미리 캐시 ===
